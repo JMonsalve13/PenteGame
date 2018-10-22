c46774190950eb7bdc67b4bcc279101757bd6dfc
Richard Sanchez <superpoptart1@gmail.com>
2018-10-22 13:43:29 -0600

    Added end window

---
+++ PenteGame/PenteGame/GameEnd.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace PenteGame
+{
+    /// <summary>
+    /// Interaction logic for GameEnd.xaml
+    /// </summary>
+    public partial class GameEnd : Window
+    {
+        public GameEnd()
+        {
+            InitializeComponent();
+        }
+
+        private void btnMainMenu_Click(object sender, RoutedEventArgs e)
+        {
+            MainMenu main = new MainMenu();
+            main.Show();
+            Close();
+        }
+
+        private void btnNewGame_Click(object sender, RoutedEventArgs e)
+        {
+            Setup setup = new Setup();
+            setup.Show();
+            Close();
+        }
+    }
+}
